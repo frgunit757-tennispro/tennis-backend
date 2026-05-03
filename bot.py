c345f524930dd908f5c5632f29f0810e38fe3f62
frgunit757-tennispro <frg.unit757@gmail.com>
2026-05-03 23:59:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN   = os.getenv("TELEGRAM_BOT_TOKEN", "ВАШ_ТОКЕН")
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8500214628:AAGInqfRQ9Bsn4cZzTLrysyrXFR9gwvMKNc")
 MINI_APP_URL = os.getenv("MINI_APP_URL", "https://ВАШ_ДОМЕН")  # URL фронтенда
 
 
